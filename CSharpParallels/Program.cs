0a0552426fcc5827890a3902772deeec907edd13
Strati <m1r0x@live.de>
2018-09-15 08:52:20 +0200

    initial commit

--- CSharpParallels/Program.cs
+++ CSharpParallels/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace CSharpParallels
 {
@@ -10,6 +11,40 @@ namespace CSharpParallels
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Synchrone Berechnung ist gestartet");
+            var watch = Stopwatch.StartNew();
+            SerialTest();
+            watch.Stop();
+            var elapsedSerial = watch.ElapsedMilliseconds;
+            Console.WriteLine("Synchrone Berechnung ist beendet");
+            Console.WriteLine("Dauer der Ausführung: " + elapsedSerial);
+            watch.Reset();
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Parallele Berechnung ist gestartet");
+            watch.Start();
+            ParallelTest();
+            watch.Stop();
+            var elapsedParallel = watch.ElapsedMilliseconds;
+            Console.WriteLine("Parallele Berechnung ist beendet");
+            Console.WriteLine("Dauer der Ausführung: " + elapsedParallel);
+            watch.Reset();
+            Console.ReadKey();
+        }
+
+        static void SerialTest()
+        {
+            double[] arr = new double[50000000];
+            for (int i = 0; i < 50000000; i++)
+                arr[i] = Math.Pow(i, 0.333) * Math.Sqrt(Math.Sin(i));
+        }
+
+        static void ParallelTest()
+        {
+            double[] arr = new double[50000000];
+            Parallel.For(0, 50000000, i =>
+            {
+                arr[i] = Math.Pow(i, 0.333) * Math.Sqrt(Math.Sin(i));
+            });
         }
     }
 }
